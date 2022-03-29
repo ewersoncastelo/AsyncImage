ac10278f2f1e3743d17f13dd3939fc9391c51667
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-03-28 21:33:10 -0400

    chore: create extension for image and icon image

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -10,13 +10,35 @@ import SwiftUI
 struct ContentView: View {
 	private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
 	
-    var body: some View {
-        AsyncImage(url: URL(string: imageURL))
-    }
+	var body: some View {
+		AsyncImage(url: URL(string: imageURL), scale: 3.0) { image in
+			image.imageModifier()
+		} placeholder: {
+			Image(systemName: "photo.circle.fill").iconModifier()
+		}
+		.padding(40)
+	}
+}
+
+// MARK: - Extensions
+extension Image {
+	func imageModifier() -> some View {
+		self
+			.resizable()
+			.scaledToFit()
+	}
+	
+	func iconModifier() -> some View {
+		self
+			.imageModifier()
+			.frame(maxWidth: 128)
+			.foregroundColor(.purple)
+			.opacity(0.5)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
