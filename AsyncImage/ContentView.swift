21440d0b9e6a197d34285b163d48b0bace1f5387
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-03-28 21:51:00 -0400

    chore: using phase for AsyncImage

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -11,12 +11,25 @@ struct ContentView: View {
 	private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
 	
 	var body: some View {
-		AsyncImage(url: URL(string: imageURL), scale: 3.0) { image in
-			image.imageModifier()
-		} placeholder: {
-			Image(systemName: "photo.circle.fill").iconModifier()
+		VStack {
+			AsyncImage(url: URL(string: imageURL), scale: 3.0) { image in
+				image.imageModifier()
+			} placeholder: {
+				Image(systemName: "photo.circle.fill").iconModifier()
+			}
+			.padding(40)
+			
+			AsyncImage(url: URL(string: imageURL), scale: 3.0) { phase in
+				if let image = phase.image {
+					image.imageModifier()
+				} else if phase.error != nil {
+					Image(systemName: "ant.circle.fill").iconModifier()
+				} else {
+					Image(systemName: "photo.circle.fill").iconModifier()
+				}
+			}
+			.padding(40)
 		}
-		.padding(40)
 	}
 }
 
